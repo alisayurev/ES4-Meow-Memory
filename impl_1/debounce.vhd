d13dd19975d8f8b66613ba8e415bed49a89aa1f4
alisayurev <153692402+alisayurev@users.noreply.github.com>
2024-12-21 15:07:15 -0800

    Update debounce.vhd

--- impl_1/debounce.vhd
+++ impl_1/debounce.vhd
@@ -3,28 +3,29 @@ use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.numeric_std.all;
 
 
---just a four bit register
-
+--four bit register
 entity debounce_input is
 port(
     clk				: in std_logic;
     input			: in std_logic_vector(3 downto 0);
-	clock_enable	: in std_logic;
+    clock_enable		: in std_logic;
     output			: out std_logic_vector(3 downto 0)
 );
 end debounce_input;
 
 
 architecture behavioral of debounce_input is 
- signal bigcounter : unsigned(24 downto 0);
+
+signal bigcounter : unsigned(24 downto 0);
+
 begin
 process(clk) 
 begin 
     if rising_edge(clk) then 
 
-				if (clock_enable = '1') then
-				output <= input;
-				end if;
+	if (clock_enable = '1') then
+		output <= input;
+	end if;
 
     end if;
 end process;
